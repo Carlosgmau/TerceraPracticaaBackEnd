37761ce378bdd9cc574cdc2d99a8d8c7b7cea49a
Carlosgmau <carlosesgmgm2003@gmail.com>
2025-12-08 17:38:30 +0100

    Update errorHandler.ts

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -3,11 +3,8 @@ import { Request, Response, NextFunction } from "express";
 export const errorHandler = (
 
   err: any,
-
   _req: Request,
-
   res: Response,
-
   _next: NextFunction
 
 ) => {
@@ -15,17 +12,12 @@ export const errorHandler = (
   console.error("Error interno:", err);
 
   if (err.status) {
-
     return res.status(err.status).json({
-
       message: err.message || "Ha ocurrido un problema."
-
     });
 
   }
-
   return res.status(500).json({
-
     message: "Algo ha pasafo en el servidor, prueba más tarde."
   });
 };
