f527c27d80ea4fd81940964a35bb38b8b1a47413
Carlosgmau <carlosesgmgm2003@gmail.com>
2025-12-08 17:46:33 +0100

    Update mongo.ts

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,28 +1,21 @@
 import { Db, MongoClient } from "mongodb";
 
 let client: MongoClient;
-
 let db: Db;
 
 
 export const connectToMongoDB = async (): Promise<void> => {
   try {
     const urlMongo = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.CLUSTER}.l2r3r2m.mongodb.net/?appName=${process.env.CLUSTER_NAME}`;
-
     client = new MongoClient(urlMongo);
-
     await client.connect();
-
     db = client.db("P3");
-
     console.log("Conexión a MongoDB lista.");
 
   } catch (err) {
 
     console.error("No se pudo conectar a Mongo:", err);
-
     process.exit(1);
-
   }
 };
 
